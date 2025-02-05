4755b0a35d9104fe0ba38d3e58ea64e07c57f99d
Eden <educasmail@gmail.com>
2025-02-05 15:29:28 +0300

    add skeleton for tasks

---
+++ src/components/Tasks/Tasks.jsx
@@ -0,0 +1,16 @@
+export default function Tasks() {
+    return <div>
+        <h2>This is the list of tasks!</h2>
+        <div>
+            <button> + Add a task</button>
+        </div>
+        <ul>
+            <li>
+                task 1
+            </li>
+            <li>
+                task 2
+            </li>
+        </ul>
+    </div>
+}
