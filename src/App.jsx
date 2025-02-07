e8b22c1e2ca93d19ceda2d51d7a0bc2428a65596
Eden <educasmail@gmail.com>
2025-02-07 20:51:35 +0300

    implement auth0 authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,23 @@
 import './App.css';
 import "tailwindcss/tailwind.css";
-import Tasks from './Tasks/Tasks';
-import Registration from './Auth/Registration';
-import NavBar from './NavBar';
+import Tasks from './components/Tasks/Tasks';
 import {
   QueryClient,
   QueryClientProvider,
-  useQuery,
 } from '@tanstack/react-query'
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import Auth from './Auth/Auth';
-
+import NavBar from './components/NavBar';
 const queryClient = new QueryClient();
 
 function App() {
+
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
         {/* <Link to="/">Auth</Link> */}
         <Routes>
-          <Route path="/" element={<Auth />} />
-          <Route path="/signup" element={<Auth type={'signup'} />} />
-          <Route path="/login" element={<Auth type={'login'} />} />
+          <Route path="/" element={<NavBar />} />
           <Route path="/tasks" element={<Tasks />} />
         </Routes>
       </Router>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,43 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 const NavBar = () => {
     const navigate = useNavigate(); // Hook for navigation
+    const { loginWithRedirect, logout, user, isAuthenticated, isLoading } = useAuth0();
 
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
     return (
         <nav className="bg-gray-800 p-4">
             <div className="max-w-7xl mx-auto flex justify-between items-center">
                 <div className="text-white font-semibold text-xl">ToDo App</div>
-                <button onClick={() => navigate("/signup")} className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">
-                    Register
-                </button>
-                <button onClick={() => navigate("/login")} className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">
-                    Login
-                </button>
-                <button onClick={() => navigate("/login")} className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">
-                    Logout
-                </button>
+
+                {isAuthenticated ? (
+                    <button
+                        onClick={() => logout({ returnTo: window.location.origin })}
+                        className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-700"
+                    >
+                        Logout
+                    </button>
+                ) : (
+                    <button
+                        onClick={() => loginWithRedirect()}
+                        className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
+                    >
+                        Logiiiin
+                    </button>
+                )}
+                {/* {isAuthenticated && (
+                    <div className="p-4 bg-gray-100 rounded shadow-md max-w-sm mx-auto">
+                        <h2 className="text-xl font-bold mb-2">User Profile</h2>
+                        <img src={user.picture} alt={user.name} className="w-16 h-16 rounded-full mb-2" />
+                        <p><strong>Name:</strong> {user.name}</p>
+                        <p><strong>Email:</strong> {user.email}</p>
+                    </div>
+                )} */}
             </div>
         </nav>
     );
