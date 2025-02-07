552da0b6b96f7b71b39afc2980f719f32f5f0a75
Eden <educasmail@gmail.com>
2025-02-07 23:08:40 +0300

    enhance nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,18 +16,24 @@ const NavBar = () => {
                 <div className="text-white font-semibold text-xl">ToDo App</div>
 
                 {isAuthenticated ? (
-                    <button
-                        onClick={() => logout({ returnTo: window.location.origin })}
-                        className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-700"
-                    >
-                        Logout
-                    </button>
+                    <span className='flex'>
+                        <span className='text-right justify-center'> <p className='text-white text-sm'>{user.name}</p>
+                            <p className='text-white text-sm'>{user.email}</p>
+                        </span>
+                        <img src={user.picture} alt={user.name} className="w-10 h-10 rounded-full mb-2 mr-10" />
+
+                        <button
+                            onClick={() => logout({ returnTo: window.location.origin })}
+                            className="justify-center bg-gray-600 text-white p-2 rounded-full hover:bg-red-700 w-16 h-10 text-sm"
+                        >Logout
+                        </button>
+                    </span>
                 ) : (
                     <button
                         onClick={() => loginWithRedirect()}
-                        className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
+                        className="justify-center bg-green-700 text-white p-2 rounded-lg hover:bg-green-900 w-16 h-10 text-sm"
                     >
-                        Logiiiin
+                        Login
                     </button>
                 )}
                 {/* {isAuthenticated && (
