9ec3259eb206a6f724fb4917918dd0828f23f8a2
Eden <educasmail@gmail.com>
2025-02-07 14:34:03 +0300

    apply routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import './App.css';
 import "tailwindcss/tailwind.css";
 import Tasks from './Tasks/Tasks';
+import Registration from './Auth/Registration';
+import NavBar from './NavBar';
 import {
   QueryClient,
   QueryClientProvider,
   useQuery,
 } from '@tanstack/react-query'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Auth from './Auth/Auth';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <h1 className='text-blue-900 text-center'>Dashboard</h1>
-      <Tasks></Tasks>
+      <Router>
+        {/* <Link to="/">Auth</Link> */}
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="/signup" element={<Auth type={'signup'} />} />
+          <Route path="/login" element={<Auth type={'login'} />} />
+          <Route path="/tasks" element={<Tasks />} />
+        </Routes>
+      </Router>
     </QueryClientProvider>
   );
 }
